aa48a92746054a03bafd55022d81aa7112c0a1b9
KRISHNA-JAIS <krishna.jaiswal@knoldus.com>
2022-07-14 12:39:50 +0530

    Added Swagger and implemented session timeout

--- src/main/java/com/knoldus/trainning/StackOverflowApplication/securityConfig/WebSecurityConfiguration.java
+++ src/main/java/com/knoldus/trainning/StackOverflowApplication/securityConfig/WebSecurityConfiguration.java
@@ -1,15 +1,16 @@
 package com.knoldus.trainning.StackOverflowApplication.securityConfig;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
 
 
 @Configuration
@@ -27,11 +28,20 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         return provider;
     }
 
+    private static final String[] AUTH_LIST = {
+            // -- swagger ui
+            "**/swagger-resources/**",
+            "/swagger-ui.html",
+            "/v2/api-docs",
+            "/webjars/**"
+    };
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http
                 .authorizeRequests()
+                .antMatchers(AUTH_LIST).permitAll()
                 .antMatchers("/","/new","/save").permitAll()
                 .anyRequest().authenticated()
                 .and()
@@ -40,6 +50,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .httpBasic();
 
     }
+
 }
 
 
