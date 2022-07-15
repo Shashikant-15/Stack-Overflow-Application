d47377f84b62af362d1e1b67d82b15e1c12ac839
Shashikant-15 <shashikant.tanti@knoldus.com>
2022-07-15 19:02:36 +0530

    some changes for branch LearnB1

--- src/main/java/com/knoldus/trainning/StackOverflowApplication/service/QuestionService.java
+++ src/main/java/com/knoldus/trainning/StackOverflowApplication/service/QuestionService.java
@@ -1,2 +1,34 @@
-package com.knoldus.trainning.StackOverflowApplication.service;public class QuestionService {
+package com.knoldus.trainning.StackOverflowApplication.service;
+
+
+import com.knoldus.trainning.StackOverflowApplication.entity.Question;
+import java.awt.print.Pageable;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+
+public class QuestionService {
+
+
+    List<Question> findAll() {
+        return null;
+    }
+
+    Page<Question> findAll(Pageable pageable) {
+        return null;
+    }
+
+    Optional<Question> findById(Long id) {
+        return Optional.empty();
+    }
+
+    Question save(Question question) {
+
+
+        return question;
+    }
+
+    void deleteById(Long id) {
+    }
+
 }
